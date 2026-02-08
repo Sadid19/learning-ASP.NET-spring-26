e29c96a83eb178a8caf6c229ef29f766a5071b1a
Md. Ibtihazzaman <mdibtihazzaman@gmail.com>
2026-02-08 15:13:00 +0600

    First commit in ASP .net repo!

---
+++ IntroMVCCore/IntroMVCCore/Controllers/PortfolioController.cs
@@ -0,0 +1,70 @@
+﻿using IntroMVCCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+/*namespace IntroMVCCore.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        public IActionResult Education()
+        {
+
+            ViewData["Name"] = "Sadid"; //Dictionary
+
+            ViewBag.Name = "Sadid";
+            ViewBag.Id = "123";
+            return View();
+        }
+        public IActionResult Education2()
+        {
+            PersonalInfo personalInfo1 = new PersonalInfo();
+            personalInfo1.Title = "SSC";
+            personalInfo1.Result = 5.00f;
+            personalInfo1.Year = 2018;
+
+            PersonalInfo personalInfo2 = new PersonalInfo()
+            {
+                Title = "HSC",
+                Result = 5.00f,
+                Year = 2020
+            };
+            PersonalInfo[] personalInfos = new PersonalInfo[] { personalInfo1, personalInfo2 };
+            return View(personalInfos); //model Binding
+        }
+    }
+}*/
+
+namespace IntroMVCCore.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Bio()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
---
+++ IntroMVCCore/IntroMVCCore/Models/PersonalInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace IntroMVCCore.Models
+{
+    public class PersonalInfo
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public float Result { get; set; }
+        public int Year { get; set; }
+
+    }
+}
